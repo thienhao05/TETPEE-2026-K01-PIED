edaf1e7085a548fb73f60746846a8b6a5123290a
Thien Hao <93728241+thienhao05@users.noreply.github.com>
2026-01-30 00:18:16 +0700

    update

--- TetPee.Repository/AppDbContext.cs
+++ TetPee.Repository/AppDbContext.cs
@@ -5,20 +5,20 @@ namespace TetPee.Repository;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) 
-        : base(options){}
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
 
-    public DbSet<User>  Users {get;set;}
-    public DbSet<Seller>  Sellers {get;set;}
-    public DbSet<Product>  Products {get;set;}
-    public DbSet<ProductStorage>  ProductStorages {get;set;}
-    public DbSet<Storage>  Storages {get;set;}
-    public DbSet<Cart>  Carts {get;set;}
-    public DbSet<Inventory>  Inventories {get;set;}
-    public DbSet<Order>  Orders {get;set;}
-    public DbSet<OrderDetail>  OrderDetails {get;set;}
-    public DbSet<ProductCategory>  ProductCategories {get;set;}
-    public DbSet<Category>  Categories {get;set;}
+    public DbSet<User> Users { get; set; }
+    public DbSet<Seller> Sellers { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductStorage> ProductStorages { get; set; }
+    public DbSet<Storage> Storages { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<Inventory> Inventories { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+    public DbSet<ProductCategory> ProductCategories { get; set; }
+    public DbSet<Category> Categories { get; set; }
     
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
