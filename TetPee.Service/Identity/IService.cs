551f53678eb6db7213bef65b83bf2102d002f4bb
Thien Hao <93728241+thienhao05@users.noreply.github.com>
2026-03-15 05:58:22 +0700

    restore --source 91899e5 .

--- TetPee.Service/Identity/IService.cs
+++
@@ -1,9 +0,0 @@
-using TetPee.Service.User;
-
-namespace TetPee.Service.Identity;
-
-public interface IService
-{
-    public Task<Response.IndentityResponse> Login(string email, string password);
-
-}
--- TetPee.Service/Identity/Service.cs
+++
@@ -1,9 +0,0 @@
-namespace TetPee.Service.Identity;
-
-public class Service : IService
-{
-    public Task<Response.IndentityResponse> Login(string email, string password)
-    {
-        throw new NotImplementedException();
-    }
-}
